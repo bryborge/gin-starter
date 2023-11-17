902c01c3372396a9f70e34ca47ca1b1f99f8719c
Bryan Borgeson <bryborge@gmail.com>
2023-11-16 18:35:37 -0800

    finish encapsulating auth in middleware

--- server/router.go
+++ server/router.go
@@ -3,13 +3,8 @@ package server
 import (
 	"gin-starter/controllers"
 	ApiV1 "gin-starter/controllers/api/v1"
-	"net/url"
-	"os"
-	"time"
+	"gin-starter/middleware"
 
-	jwtmiddleware "github.com/auth0/go-jwt-middleware/v2"
-	"github.com/auth0/go-jwt-middleware/v2/jwks"
-	"github.com/auth0/go-jwt-middleware/v2/validator"
 	"github.com/gin-gonic/gin"
 	adapter "github.com/gwatts/gin-adapter"
 )
@@ -24,21 +19,9 @@ func NewRouter() *gin.Engine {
 	publicRoutes := router.Group("/")
 	publicRoutes.GET("/health", new(controllers.HealthController).Status)
 
-	// TODO: Encapsulate and move to auth middleware
-	issuerURL, _ := url.Parse(os.Getenv("AUTH0_ISSUER_URL"))
-	audience := os.Getenv("AUTH0_AUDIENCE")
-	provider := jwks.NewCachingProvider(issuerURL, time.Duration(5*time.Minute))
-	jwtValidator, _ := validator.New(
-		provider.KeyFunc,
-		validator.RS256,
-		issuerURL.String(),
-		[]string{audience},
-	)
-	jwtMiddleware := jwtmiddleware.New(jwtValidator.ValidateToken)
-
 	// Protected routes
 	protectedRoutes := router.Group("/api/v1")
-	protectedRoutes.Use(adapter.Wrap(jwtMiddleware.CheckJWT))
+	protectedRoutes.Use(adapter.Wrap(middleware.EnsureValidToken()))
 	protectedRoutes.GET("/pokemon", ApiV1.GetPokemon)
 
 	return router
